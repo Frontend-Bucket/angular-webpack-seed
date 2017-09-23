95dafa04ba4de4af1691f86c6d0d4f7cc1d94317
Ashad Shanto <mohammad.shanto@gmail.com>
2017-09-23 23:31:11 +0600

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -37,37 +37,39 @@ var webpackConfig = {
 };
 
 var defaultConfig = {
-  devtool: 'source-map',
+    devtool: 'source-map',
 
-  output: {
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
-  },
+    output: {
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
+    },
 
-  resolve: {
-    extensions: [ '.ts', '.js' ],
-    modules: [ path.resolve(__dirname, 'node_modules') ]
-  },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [path.resolve(__dirname, 'node_modules')]
+    },
 
-  devServer: {
-    historyApiFallback: true,
-    watchOptions: { aggregateTimeout: 300, poll: 1000 },
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    devServer: {
+        historyApiFallback: true,
+        watchOptions: { aggregateTimeout: 300, poll: 1000 },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
+    },
+
+    node: {
+        global: true,
+        crypto: 'empty',
+        __dirname: true,
+        __filename: true,
+        process: true,
+        Buffer: false,
+        clearImmediate: false,
+        setImmediate: false
     }
-  },
-
-  node: {
-    global: true,
-    crypto: 'empty',
-    __dirname: true,
-    __filename: true,
-    process: true,
-    Buffer: false,
-    clearImmediate: false,
-    setImmediate: false
-  }
 };
+
+module.exports = webpackMerge(defaultConfig, webpackConfig);
